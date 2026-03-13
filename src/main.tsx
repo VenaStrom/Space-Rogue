cc77dbbee05d4f6378b7c14223cfb5009fc8389e
VenaStrom <strom.vena+gh@gmail.com>
2026-03-13 23:15:28 +0100

    actually implement context providers... oopsies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import "./global.tw.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app.tsx";
+import { GameStateProvider, MetaStateProvider } from "./context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MetaStateProvider>
+      <GameStateProvider>
+        <App />
+      </GameStateProvider>
+    </MetaStateProvider>
   </StrictMode>,
 );
