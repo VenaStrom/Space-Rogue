b35645edf879c76f11d3efa8a173de5c5107536c
VenaStrom <vena.strom+github@pm.me>
2026-07-30 22:56:40 +0200

    bump deps

--- src/types/_type-guards.ts
+++ src/types/_type-guards.ts
@@ -1,7 +1,7 @@
 import { Route } from "./_consts";
 import type { JSONValue } from "./_types";
 
-function isStandardObject(value: unknown): value is Record<string, unknown> {
+function isObj(value: unknown): value is Record<string, unknown> {
   if (typeof value !== "object" || value === null) {
     console.info(`Value is not an object: ${(value as string).toString()}`);
     return false;
